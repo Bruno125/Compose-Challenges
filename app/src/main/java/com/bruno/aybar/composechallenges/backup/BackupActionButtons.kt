f1f430b20f5b4570d3858a3f9d17167658afe05a
Bruno Aybar <Bruno125@users.noreply.github.com>
2021-02-24 12:28:11 -0500

    [ALPHA 11] Update transitions API for BackupScreen and BatmanScreen (#10)
    * Update to alpha11 - part3: update transitions API for BackupScreen
    
    * Simplify state holder class
    
    * Standardize UI properties creation
    
    * Update to alpha11 - part4: update transitions API for BatmanScreen
    
    * Remove custom animation holder. No longer needed with new APIs
    
    * Revert misc changes
    
    * Fix issue with batman size transition
    
    * Update ui properties to use states

--- app/src/main/java/com/bruno/aybar/composechallenges/backup/BackupActionButtons.kt
+++ app/src/main/java/com/bruno/aybar/composechallenges/backup/BackupActionButtons.kt
@@ -8,35 +8,38 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.OutlinedButton
 import androidx.compose.runtime.*
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
-import com.bruno.aybar.composechallenges.common.AnimationStateHolder
-import com.bruno.aybar.composechallenges.common.transition
 import com.bruno.aybar.composechallenges.ui.buttonHeight
 
-
-private val backupButtonAlpha = FloatPropKey("Backup button alpha")
-private val backupButtonSizeMultiplier = FloatPropKey("Backup button size multiplier")
-
-private val cancelButtonAlpha = FloatPropKey("Cancel button alpha")
-private val cancelButtonWidth = IntPropKey("Cancel button width")
+private class ButtonsUiProperties(
+    backupButtonAlpha: State<Float>,
+    backupButtonSizeMultiplier: State<Float>,
+    cancelButtonAlpha: State<Float>,
+    cancelButtonWidth: State<Int>,
+) {
+    val backupButtonAlpha: Float by backupButtonAlpha
+    val backupButtonSizeMultiplier: Float by backupButtonSizeMultiplier
+    val cancelButtonAlpha: Float by cancelButtonAlpha
+    val cancelButtonWidth: Int by cancelButtonWidth
+}
 
 enum class ButtonsState {
     CreateBackup,
     Cancel
 }
 
-class BackupActionButtonState: AnimationStateHolder<ButtonsState>(
-    initialState = ButtonsState.CreateBackup
-){
+class BackupActionButtonState {
+    var state = ButtonsState.CreateBackup; private set
 
     fun update(ui: BackupUi) {
-        animateTo(newState = when(ui) {
+        state = when(ui) {
             is BackupUi.RequestBackup -> ButtonsState.CreateBackup
             else -> ButtonsState.Cancel
-        })
+        }
     }
 
 }
@@ -49,26 +52,23 @@ fun BackupActionButtons(
     onCancel: ()->Unit,
     modifier: Modifier
 ) {
-    val animationState = remember { BackupActionButtonState() }
+    val stateHolder = remember { BackupActionButtonState() }
 
-    animationState.update(ui)
+    stateHolder.update(ui)
 
-    val transition = transition(
-        definition = AnimateButtonsTransition,
-        stateHolder = animationState
-    )
+    val properties = buildUiProperties(stateHolder.state)
 
     Box(modifier) {
 
         BackupButton(
             onClick = onBackup,
-            transition = transition,
+            properties = properties,
             modifier = Modifier.align(Alignment.Center)
         )
 
         CancelButton(
             onClick = onCancel,
-            transition = transition,
+            properties = properties,
             modifier = Modifier.align(Alignment.Center)
         )
 
@@ -76,15 +76,15 @@ fun BackupActionButtons(
 }
 
 @Composable
-private fun BackupButton(onClick: ()->Unit, modifier: Modifier, transition: TransitionState) {
-    if(transition[backupButtonAlpha] == 0f) return
+private fun BackupButton(onClick: ()->Unit, properties: ButtonsUiProperties, modifier: Modifier) {
+    if(properties.backupButtonAlpha == 0f) return
 
     Button(
         onClick = onClick,
         modifier = modifier
-            .alpha(transition[backupButtonAlpha])
-            .preferredWidth(240.dp * transition[backupButtonSizeMultiplier])
-            .preferredHeight(buttonHeight * transition[backupButtonSizeMultiplier]),
+            .alpha(properties.backupButtonAlpha)
+            .preferredWidth(240.dp * properties.backupButtonSizeMultiplier)
+            .preferredHeight(buttonHeight * properties.backupButtonSizeMultiplier),
         shape = RoundedCornerShape(10.dp)
     ) {
         Text(text = "Create Backup")
@@ -92,49 +92,74 @@ private fun BackupButton(onClick: ()->Unit, modifier: Modifier, transition: Tran
 }
 
 @Composable
-private fun CancelButton(onClick: ()->Unit, modifier: Modifier, transition: TransitionState) {
-    if(transition[cancelButtonAlpha] == 0f) return
+private fun CancelButton(onClick: ()->Unit, properties: ButtonsUiProperties, modifier: Modifier) {
+    if(properties.cancelButtonAlpha == 0f) return
 
     OutlinedButton(
         onClick = onClick,
         modifier = modifier
-            .alpha(transition[cancelButtonAlpha])
-            .preferredWidth(transition[cancelButtonWidth].dp)
+            .alpha(properties.cancelButtonAlpha)
+            .preferredWidth(properties.cancelButtonWidth.dp)
             .preferredHeight(buttonHeight),
     ) {
         Text(text = "Cancel")
     }
 }
 
-private val AnimateButtonsTransition = transitionDefinition<ButtonsState> {
+@Composable
+private fun buildUiProperties(state: ButtonsState): ButtonsUiProperties {
+
+    val transition: Transition<ButtonsState> = updateTransition(state)
     val animationDuration = DefaultDurationMillis
 
-    state(ButtonsState.CreateBackup) {
-        this[backupButtonAlpha] = 1f
-        this[backupButtonSizeMultiplier] = 1f
+    val backupButtonAlpha = transition.animateFloat(
+        transitionSpec = {
+            if(ButtonsState.CreateBackup isTransitioningTo ButtonsState.Cancel) {
+                tween(delayMillis = animationDuration, easing = LinearEasing)
+            } else {
+                tween(easing = LinearEasing)
+            }
+        },
+        targetValueByState = { if(it == ButtonsState.CreateBackup) 1f else 0f }
+    )
 
-        this[cancelButtonAlpha] = 0f
-        this[cancelButtonWidth] = 180
-    }
-    state(ButtonsState.Cancel) {
-        this[backupButtonAlpha] = 0f
-        this[backupButtonSizeMultiplier] = 1f
-        this[cancelButtonAlpha] = 0.6f
-        this[cancelButtonWidth] = 150
-    }
+    val backupButtonSizeMultiplier = transition.animateFloat(
+        transitionSpec = {
+            if(ButtonsState.CreateBackup isTransitioningTo ButtonsState.Cancel) {
+                keyframes { 0.95f at animationDuration / 2 }
+            } else {
+                keyframes { 1.05f at animationDuration / 2 }
+            }
+        },
+        targetValueByState = { 1f }
+    )
 
-    transition(fromState = ButtonsState.CreateBackup, toState = ButtonsState.Cancel) {
-        backupButtonSizeMultiplier using keyframes { 0.95f at animationDuration / 2 }
-        backupButtonAlpha using tween(delayMillis = animationDuration, easing = LinearEasing)
-        cancelButtonAlpha using tween(delayMillis = animationDuration, easing = LinearEasing)
-        cancelButtonWidth using tween(delayMillis = animationDuration + 150)
-    }
+    val cancelButtonAlpha = transition.animateFloat(
+        transitionSpec = {
+            if(ButtonsState.CreateBackup isTransitioningTo ButtonsState.Cancel) {
+                tween(delayMillis = animationDuration, easing = LinearEasing)
+            } else {
+                tween(easing = LinearEasing)
+            }
+        },
+        targetValueByState = { if(it == ButtonsState.CreateBackup) 0f else 0.6f }
+    )
 
-    transition(fromState = ButtonsState.Cancel, toState = ButtonsState.CreateBackup) {
-        backupButtonAlpha using tween(easing = LinearEasing)
-        cancelButtonAlpha using tween(easing = LinearEasing)
-        cancelButtonWidth using tween()
-        backupButtonSizeMultiplier using keyframes { 1.05f at animationDuration / 2 }
-    }
+    val cancelButtonWidth = transition.animateInt(
+        transitionSpec = {
+            if(ButtonsState.CreateBackup isTransitioningTo ButtonsState.Cancel) {
+                tween(delayMillis = animationDuration + 150)
+            } else {
+                tween()
+            }
+        },
+        targetValueByState = { if(it == ButtonsState.CreateBackup) 180 else 150 }
+    )
 
+    return ButtonsUiProperties(
+        backupButtonAlpha = backupButtonAlpha,
+        backupButtonSizeMultiplier = backupButtonSizeMultiplier,
+        cancelButtonAlpha = cancelButtonAlpha,
+        cancelButtonWidth = cancelButtonWidth
+    )
 }
--- app/src/main/java/com/bruno/aybar/composechallenges/backup/BackupBody.kt
+++ app/src/main/java/com/bruno/aybar/composechallenges/backup/BackupBody.kt
@@ -3,28 +3,29 @@ package com.bruno.aybar.composechallenges.backup
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
-import com.bruno.aybar.composechallenges.common.AnimationStateHolder
-import com.bruno.aybar.composechallenges.common.transition
 import com.bruno.aybar.composechallenges.ui.typography
 
 
-class BodyAnimationState: AnimationStateHolder<BodyState>(BodyState.LAST_BACKUP) {
+class BodyAnimationState {
 
+    var state: BodyState = BodyState.LAST_BACKUP; private set
     var cachedBackupDate: String = ""; private set
     var cachedProgress: Int = 0; private set
 
     fun update(ui: BackupUi) {
         updateCachedValues(ui)
-        animateTo(newState = when(ui) {
+        state = when(ui) {
             is BackupUi.RequestBackup -> BodyState.LAST_BACKUP
             is BackupUi.BackupInProgress,
             is BackupUi.BackupCompleted-> BodyState.UPLOADING
-        })
+        }
     }
 
     private fun updateCachedValues(ui: BackupUi) {
@@ -37,106 +38,148 @@ class BodyAnimationState: AnimationStateHolder<BodyState>(BodyState.LAST_BACKUP)
 }
 
 @Composable
-fun Body(state: BackupUi, modifier: Modifier) {
+fun BackupBody(state: BackupUi, modifier: Modifier) {
 
     val bodyState = remember { BodyAnimationState() }
 
     bodyState.update(state)
 
-    val transition = transition(
-        definition = bodyTransition,
-        stateHolder = bodyState
-    )
+    val properties = buildUiProperties(bodyState.state)
 
     Box(modifier) {
         LastBackup(
             modifier = Modifier.align(Alignment.Center),
             date = bodyState.cachedBackupDate,
-            transitionState = transition)
+            properties = properties,
+        )
 
         UploadingHint(
             modifier = Modifier.align(Alignment.Center),
             progress = bodyState.cachedProgress,
-            transitionState = transition
+            properties = properties,
         )
     }
 }
 
 @Composable
-fun LastBackup(modifier: Modifier, date: String, transitionState: TransitionState) {
-    Column(modifier.alpha(transitionState[lastBackupAlpha]),
+private fun LastBackup(modifier: Modifier, date: String, properties: BackupBodyUiProperties) {
+    Column(modifier.alpha(properties.lastBackupAlpha),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("last backup", style = typography.caption)
-        Spacer(Modifier.height(transitionState[lastBackupHintOffset].dp))
+        Spacer(Modifier.height(properties.lastBackupHintOffset.dp))
         Text(text = date, style = typography.body1)
-        Spacer(Modifier.height(transitionState[lastBackupDateOffset].dp))
+        Spacer(Modifier.height(properties.lastBackupDateOffset.dp))
     }
 }
 
 
 @Composable
-fun UploadingHint(modifier: Modifier, progress: Int, transitionState: TransitionState) {
-    Column(modifier.alpha(transitionState[uploadingAlpha]),
+private fun UploadingHint(modifier: Modifier, progress: Int, properties: BackupBodyUiProperties) {
+    Column(modifier.alpha(properties.uploadingAlpha),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(Modifier.height(transitionState[uploadingOffset].dp))
+        Spacer(Modifier.height(properties.uploadingOffset.dp))
         Text("uploading file", style = typography.subtitle1)
         Text(text = "$progress%", style = typography.h1)
     }
 }
 
+private class BackupBodyUiProperties(
+    val lastBackupAlpha: Float,
+    val lastBackupHintOffset: Int,
+    val lastBackupDateOffset: Int,
 
-private val lastBackupAlpha = FloatPropKey()
-private val lastBackupHintOffset = IntPropKey()
-private val lastBackupDateOffset = IntPropKey()
-
-private val uploadingAlpha = FloatPropKey()
-private val uploadingOffset = IntPropKey()
-
-private val successAlpha = FloatPropKey()
-private val successOffset = IntPropKey()
+    val uploadingAlpha: Float,
+    val uploadingOffset: Int,
+)
 
 enum class BodyState {
     LAST_BACKUP, UPLOADING
 }
 
-private val bodyTransition = transitionDefinition<BodyState> {
-    state(BodyState.LAST_BACKUP) {
-        this[lastBackupAlpha] = 1f
-        this[lastBackupHintOffset] = 0
-        this[lastBackupDateOffset] = 0
-        this[uploadingAlpha] = 0f
-        this[uploadingOffset] = 30
-        this[successAlpha] = 0f
-        this[successOffset] = 0
-    }
-    state(BodyState.UPLOADING) {
-        this[lastBackupAlpha] = 0f
-        this[lastBackupHintOffset] = 15
-        this[lastBackupDateOffset] = 40
-        this[uploadingAlpha] = 1f
-        this[uploadingOffset] = 0
-        this[successAlpha] = 0f
-        this[successOffset] = 0
-    }
-
+@Composable
+private fun buildUiProperties(state: BodyState): BackupBodyUiProperties {
+    val transition: Transition<BodyState> = updateTransition(state)
     val duration = AnimationConstants.DefaultDurationMillis
 
-    transition(fromState = BodyState.LAST_BACKUP, toState = BodyState.UPLOADING) {
-        lastBackupAlpha using tween(easing = LinearOutSlowInEasing)
-        lastBackupHintOffset using tween(easing = LinearOutSlowInEasing)
-        lastBackupDateOffset using tween(easing = LinearOutSlowInEasing)
-        uploadingAlpha using tween(delayMillis = duration)
-        uploadingOffset using tween(delayMillis = duration)
-    }
-    transition(fromState = BodyState.UPLOADING, toState = BodyState.LAST_BACKUP) {
-        uploadingAlpha using tween()
-        uploadingOffset using tween()
-        lastBackupAlpha using tween(delayMillis = duration)
-        lastBackupHintOffset using tween(delayMillis = duration)
-        lastBackupDateOffset using tween(delayMillis = duration)
-    }
+    val lastBackupAlpha: Float by transition.animateFloat(
+        transitionSpec = {
+            when {
+                BodyState.LAST_BACKUP isTransitioningTo BodyState.UPLOADING -> tween(easing = LinearOutSlowInEasing)
+                else -> tween(delayMillis = duration)
+            }
+        },
+        targetValueByState = {
+            when(it) {
+                BodyState.LAST_BACKUP -> 1f
+                BodyState.UPLOADING -> 0f
+            }
+        }
+    )
+    val lastBackupHintOffset: Int by transition.animateInt(
+        transitionSpec = {
+            when {
+                BodyState.LAST_BACKUP isTransitioningTo BodyState.UPLOADING -> tween(easing = LinearOutSlowInEasing)
+                else -> tween(delayMillis = duration)
+            }
+        },
+        targetValueByState = {
+            when(it) {
+                BodyState.LAST_BACKUP -> 0
+                BodyState.UPLOADING -> 15
+            }
+        }
+    )
+    val lastBackupDateOffset: Int by transition.animateInt(
+        transitionSpec = {
+            when {
+                BodyState.LAST_BACKUP isTransitioningTo BodyState.UPLOADING -> tween(easing = LinearOutSlowInEasing)
+                else -> tween(delayMillis = duration)
+            }
+        },
+        targetValueByState = {
+            when(it) {
+                BodyState.LAST_BACKUP -> 0
+                BodyState.UPLOADING -> 40
+            }
+        }
+    )
+    val uploadingAlpha: Float by transition.animateFloat(
+        transitionSpec = {
+            when {
+                BodyState.LAST_BACKUP isTransitioningTo BodyState.UPLOADING -> tween(delayMillis = duration)
+                else -> tween()
+            }
+        },
+        targetValueByState = {
+            when(it) {
+                BodyState.LAST_BACKUP -> 0f
+                BodyState.UPLOADING -> 1f
+            }
+        }
+    )
+    val uploadingOffset: Int by transition.animateInt(
+        transitionSpec = {
+            when {
+                BodyState.LAST_BACKUP isTransitioningTo BodyState.UPLOADING -> tween(delayMillis = duration)
+                else -> tween()
+            }
+        },
+        targetValueByState = {
+            when(it) {
+                BodyState.LAST_BACKUP -> 30
+                BodyState.UPLOADING -> 0
+            }
+        }
+    )
+    return BackupBodyUiProperties(
+        lastBackupAlpha = lastBackupAlpha,
+        lastBackupHintOffset = lastBackupHintOffset,
+        lastBackupDateOffset = lastBackupDateOffset,
+        uploadingAlpha = uploadingAlpha,
+        uploadingOffset = uploadingOffset,
+    )
 }
